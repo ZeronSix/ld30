8c1a73d4e387c3aad1377d3bc65d5b609d0fb5cb
Vyacheslav Zeronov <zeronsix@gmail.com>
2014-08-23 18:35:09 +0600

    Added camera controller.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    public float Speed;
+
+    void Update()
+    {
+        if (Input.GetButton("DragMouse"))
+        {
+            UpdateMovement(new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")));
+        }
+    }
+
+    private void UpdateMovement(Vector2 input)
+    {
+        transform.Translate(input * Speed, Space.Self);
+    }
+}
