bb94d4f0bb4fb396359410df32d6084eb1aa1b55
TideSofDarK <tidesmain@gmail.com>
2014-08-23 22:57:18 +0600

    new models and textures

--- Assets/Scripts/Sun.cs
+++ Assets/Scripts/Sun.cs
@@ -5,6 +5,9 @@ public class Sun : MonoBehaviour {
 
 	private GameController gameController;
 
+	public float galacticLightRange = 3.0f;
+	public float systemLightRange = 300f;
+
 	void Start () {
 		gameController = GameObject.FindWithTag ("GameController").GetComponent<GameController> ();
 
@@ -28,12 +31,16 @@ public class Sun : MonoBehaviour {
 			}
 		}	
 
+		light.range = gameController.gameState == GameController.GameState.GALACTIC_VIEW ? galacticLightRange : systemLightRange;
+
 		if (gameController.gameState == GameController.GameState.SYSTEM_VIEW && gameController.selectedSystem != this) {
 			//transform.localScale = Vector3.Lerp(transform.localScale, Vector3.zero, 15f * Time.deltaTime);		
 			renderer.enabled = false;
+			light.enabled = false;
 		} else {
 			//transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one, 15f * Time.deltaTime);	
 			renderer.enabled = true;
+			light.enabled = true;
 		}
 
 		transform.RotateAround (transform.position, Vector3.forward, 9 * Time.deltaTime);
