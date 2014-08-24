93105b8f20547be7baf36c25298a126dbe517c58
TideSofDarK <tidesmain@gmail.com>
2014-08-24 21:40:01 +0600

    early version of laser

---
+++ Assets/Scripts/LaserGun.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Timers;
+public class LaserGun : MonoBehaviour {
+
+	public float laserTime = 1.0f;
+
+	public Color laserBeamColor = Color.red;
+
+	private LineRenderer lineRenderer;
+	
+	void Start () {
+		lineRenderer = GetComponent<LineRenderer> ();
+
+		lineRenderer.enabled = false;
+	}
+
+	void Update () {
+		lineRenderer.material.color = laserBeamColor;
+
+		//if (Input.GetKeyDown (KeyCode.Space)) Attack (new Vector3(10f, 0f, 10f));
+	}
+
+	public void Attack(Vector3 target) {
+		lineRenderer.enabled = true;
+		lineRenderer.SetPosition (1, target);
+		Invoke ("HideLaser", laserTime);
+	}
+
+	private void HideLaser() {
+		lineRenderer.enabled = false;
+	}
+}
