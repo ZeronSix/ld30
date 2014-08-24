2e388442dcf9a12da9cdc48d8144ce74f0b83294
Vyacheslav Zeronov <zeronsix@gmail.com>
2014-08-24 21:42:15 +0600

    Fixed HealthBar.

--- Assets/Scripts/UnitHealthBar.cs
+++ Assets/Scripts/UnitHealthBar.cs
@@ -9,11 +9,13 @@ public class UnitHealthBar : MonoBehaviour
 
     private Transform _background;
     private Transform _foreground;
+    private Vector3 _deltaPos;
 
     void Awake()
     {
         _background = transform.Find("Background");
         _foreground = transform.Find("Foreground");
+        _deltaPos = transform.position - Unit.transform.position;
 
         _background.localScale = new Vector3(Scale, 1, 1);
         _foreground.localPosition = new Vector3(_background.localPosition.x - Scale / 2 * UnitSize, _foreground.localPosition.y, _foreground.localPosition.z);
@@ -21,6 +23,7 @@ public class UnitHealthBar : MonoBehaviour
 
     void Update()
     {
+        transform.position = Unit.transform.position + _deltaPos;
         _foreground.localScale = new Vector3(Scale * Unit.Health/Unit.MaxHealth, 1, 1);
     }
 }
