fdfe80d802fefbdfe7879cf8d96b14471b3f3137
TideSofDarK <tidesmain@gmail.com>
2014-08-25 22:59:16 +0600

    captions arrived

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -6,17 +6,30 @@ using System.Linq;
 
 public class Planet : MonoBehaviour {
 
+	public GameObject captionPrefab;
+
 	public GameObject orbit;
 	public Vector3 orbitScale;
     
 	public float realScale = 1f;
 
+	private TextMesh caption;
+
 	void Start () {
+		GenerateName ();
+
+		caption = ((GameObject)Instantiate (captionPrefab)).GetComponent<TextMesh>();
+		caption.transform.parent = transform;
+		caption.transform.localPosition = new Vector3 (0f, 0.6549172f, -0.5f);
+		caption.transform.localScale = Vector3.one * 0.009f;
+		caption.text = name;
 	}
 	
 	void Update () {
 		transform.RotateAround (transform.position, Vector3.forward, 14 * Time.deltaTime);
 		transform.RotateAround (transform.parent.position, Vector3.forward, 0.3f * 25f * Time.deltaTime / (Vector3.Distance(transform.position, transform.parent.position) * 0.05f));
+
+		caption.transform.rotation = Quaternion.identity;
 
 		orbit.transform.RotateAround (orbit.transform.position, Vector3.forward, orbitScale.x * 3 * Time.deltaTime);
 	}
@@ -29,4 +42,13 @@ public class Planet : MonoBehaviour {
 //                gc.SelectedUnit.Target = transform;
         }
     }
+	
+	public void GenerateName() {
+		int num = Random.Range(0, 25);
+		int num2 = Random.Range(0, 25);
+		char let = (char)('a' + num);
+
+		name = ((char)('a' + num)).ToString() + ((char)('a' + num2)).ToString() + "-" + Random.Range (0, 9).ToString() + Random.Range (0, 9).ToString();
+		name = name.ToUpper ();
+	}
 }
