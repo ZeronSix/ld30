8236ad9139e8912f8b3f6467a995d53d2a0593f3
Vyacheslav Zeronov <zeronsix@gmail.com>
2014-08-25 19:56:27 +0600

    Tag fix.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,22 +1,88 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 using System.Collections;
 
-public class GameController : MonoBehaviour {
-
+public class GameController : MonoBehaviour 
+{
     public Unit SelectedUnit;
     public float UnitMoveSpeed;
     public float AttackDuration;
     public bool Busy = false;
 
+    public GameObject[] Planets;
+    public ScreenFader Fader;
+    public int PlayerReinforcements = 5;
+    public float PlayerUnitCount = 0;
+    public int TurnCount = 0;
+
     private Grid _grid;
 	
-	void Start ()
+	void Awake()
 	{
 	    _grid = Grid.Get();
+        StartMission();
+
+	    var units = GameObject.FindGameObjectsWithTag("Unit");
+	    foreach (var unit in units)
+	    {
+	        if (unit.GetComponent<Unit>().BattleSide == BattleSide.Humans)
+	        {
+	            PlayerUnitCount++;
+	        }
+	    }
+	}
+
+	void Update () 
+    {
+
+	    var win = true;
+	    foreach (var planet in Planets)
+	    {
+	        if (planet.GetComponent<PlanetTrigger>().ContestedBy != BattleSide.Humans)
+	            win = false;
+	    }
+
+	    if (win)
+	    {
+	        // TODO: win
+            Fader.EndScene("HUMAN VICTORY!");
+	    }
+
+	    if (PlayerReinforcements == 0 && PlayerUnitCount == 0)
+	    {
+	        // TODO: fail
+            Fader.EndScene("ALIENS VICTORY!");
+	    }
 	}
 
     public static GameController Get()
     {
         return GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+    }
+
+    public void NextTurn()
+    {
+        TurnCount++;
+
+        // TODO: AI Turn
+        Busy = false;
+
+        TacticalSceneGUI.Get().ReinforcementsAvailable = true;
+
+        foreach (var unit in GameObject.FindGameObjectsWithTag("Unit"))
+        {
+            unit.GetComponent<Unit>().PrepareForNextTurn();
+
+            var ai = unit.GetComponent<AIUnitController>();
+            if (ai != null)
+            {
+                ai.DoTurn();
+            }
+        }
+    }
+
+    public void StartMission()
+    {
+        Planets = GameObject.FindGameObjectsWithTag("Planet");
     }
 }
---
+++ Assets/Scripts/TacticalCameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TacticalCameraController : MonoBehaviour 
+{
+    public float Speed;
+
+    private void Update()
+    {
+        if (Input.GetButton("DragMouse"))
+        {
+            UpdateMovement(new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")));
+        }
+    }
+
+    private void UpdateMovement(Vector2 input)
+    {
+        transform.Translate(-input * Speed * Time.deltaTime, Space.Self);
+    }
+}
